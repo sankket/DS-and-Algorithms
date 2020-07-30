f74f60c50d2ccd0c0f0da71185fe1e478979d30e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-30 15:11:46 +0530

    Code Updated

--- Array/Google_Problem.py
+++ Array/Google_Problem.py
@@ -1,1 +1,11 @@
+array = [1,2,3,4,5]
 
+sum = 32
+def eleSum(arr,sum):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] + arr[j] == sum :
+                return print("Yes")
+    return print("NOPE")
+
+eleSum(array,sum)
