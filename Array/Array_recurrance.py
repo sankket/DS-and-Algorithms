4b9f3ec1df9c6bbc04a4d9d11d394fc880621204
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-30 15:08:40 +0530

    Code Updated

--- Array/Array_recurrance.py
+++ Array/Array_recurrance.py
@@ -1,1 +1,17 @@
+#Print the element which occured more than one time in array.
+def First_rec(array):
+    dictionary = dict()
+
+    for i in array:
+        if i in dictionary:
+            return i
+        else:
+            dictionary[i] = True
+    return None
+
+
+array = [2, 1, 4, 1, 6, 5, 1, 4]
+
+print(First_rec(array))
+
 
