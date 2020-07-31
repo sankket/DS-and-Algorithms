6fc20e0adaf1c4c58feb6ce6073217693f714ac5
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-31 15:22:09 +0530

    Update O(1).py

--- Big-O/O(1).py
+++ Big-O/O(1).py
@@ -1,1 +1,18 @@
+import time
 
+nemo = ["sane", "mane", "salah", "nemo"]
+
+
+def findnemo(array):
+    start = time.time()
+    for i in range(len(nemo)):
+        print("Running")
+        if (array[i] == "nemo"):
+            print("Found Nemo")
+            break
+    end = time.time()
+
+    print(f"Runtime of the program is {end - start}" + "miliseconds")
+
+
+findnemo(nemo)
