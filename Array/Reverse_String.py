46a442054c0b1091fdbd37abd02c72d0387aed9e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-31 15:09:49 +0530

    Code Updated

--- Array/Reverse_String.py
+++ Array/Reverse_String.py
@@ -1,1 +1,20 @@
+string1 = str(input("Enter a string to reverse :"))
+
+
+def reverse_string(string):
+
+    for i in string:
+        string = i + string
+    return string
+
+
+print("The original string : ", string1)
+
+print("The reverse of the string: ", reverse_string(string1))
+
+# Second Method to reverse array is to just array slicing in reverse order.
+string2 = "my name is godbole"
+
+print(string2[::-1])
+
 
