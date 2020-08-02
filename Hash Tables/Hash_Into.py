1b416ecc06039f792939a1b2d78409593adf506d
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-02 14:59:54 +0530

    Code Updated.

--- Hash Tables/Hash_Into.py
+++ Hash Tables/Hash_Into.py
@@ -1,1 +1,14 @@
+# In python dictionaries are implemented as hash table.
+# hash function is used for converting the keys into hashes which are unique for each key.
+# Hashes are then mapped to the buckets and the key value pair is stored in that bucket.
+
+dictionary = dict()
+dictionary = {'one': 1, 'two': 2, 'three' : 3, 'four' : 4}
+
+print(dictionary)
+
+print(dictionary.keys())
+
+print(dictionary.values())
+
 
