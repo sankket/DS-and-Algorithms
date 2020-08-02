b1061ad8114c974af977c340d73fd5feff93017e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-02 14:54:56 +0530

    Update O(n).py

--- Big-O/O(n).py
+++ Big-O/O(n).py
@@ -1,1 +1,28 @@
+import time
 
+nemo = ['nemo']
+everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla']
+large = ['nemo' for i in range(100000)]
+def find_nemo(array):
+    t0 = time.time()
+    for i in range(0,len(array)):
+        if array[i] == 'nemo':
+            print("Found Nemo!!")
+    t1 = time.time()
+    print(f'The search took {t1-t0} seconds.')
+find_nemo(nemo)
+find_nemo(everyone)
+find_nemo(large)
+
+
+def funchallenge(input):
+    temp = 10 
+    temp = temp +50 
+    for i in range(len(input)):
+        var = True 
+        a += 1 
+    return a 
+
+funchallenge(nemo)
+funchallenge(everyone)
+funchallenge(large)
