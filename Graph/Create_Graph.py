6ab8c8fc6c8cf19b54123e4965b7e2c81e7a12c4
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-07 18:23:09 +0530

    Code Updated.

--- Graph/Create_Graph.py
+++ Graph/Create_Graph.py
@@ -1,1 +1,36 @@
+# graph is non linear data structure in which nodes are connected to each other through the edges.
+# They can be represented in 3 ways - edge list, adjacency list and adjacency matrix.
+
+class Graph:
+    def __init__(self):
+        self.num_of_nodes = 0
+        self.adjacency_list = {}
+
+    def insert_node(self, data):
+        if data not in self.adjacency_list:
+            self.adjacency_list[data] = []
+            self.num_of_nodes += 1
+            return self
+
+    def insert_edge(self, vertex1, vertex2):
+        if vertex2 not in self.adjacency_list[vertex1]:
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return self
+        return "The edge is already existed."
+
+    def show_connection(self):
+        for node in self.adjacency_list:
+            print(f'{node} -----> {" ".join(map(str, self.adjacency_list[node]))}')
+
+my_graph = Graph()
+my_graph.insert_node(1)
+my_graph.insert_node(2)
+my_graph.insert_node(3)
+my_graph.insert_edge(2, 3)
+my_graph.insert_edge(1, 3)
+my_graph.insert_edge(3, 2)
+my_graph.show_connection()
+
+
 
