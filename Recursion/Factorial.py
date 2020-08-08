581b67e2c98cf526f6aed1dd86810419b47db713
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-08 15:40:33 +0530

    Iterative method updated.

--- Recursion/Factorial.py
+++ Recursion/Factorial.py
@@ -1,1 +1,12 @@
+# Finding the factorial of the number.
+# The Iterative Method
 
+
+def factorial(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact = fact * i
+    return fact
+
+
+print("The Answer by Iteration : ", factorial(5))
