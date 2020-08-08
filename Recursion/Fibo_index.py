3e04d43c7ca39acbd6512aacfa1148eb17e91ef5
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-08 15:43:36 +0530

    Recursive method added.

--- Recursion/Fibo_index.py
+++ Recursion/Fibo_index.py
@@ -1,1 +1,11 @@
+# The Recursive Method.
+# Fibonacci Series - 0 1 1 2 3 5 8 13 21 34 55 . . . .
+def fibonacci(index):
+    if index == 0:
+        return 0
+    elif index == 1:
+        return 1
+    else:
+        return fibonacci(index-1) + fibonacci(index-2)
 
+print(fibonacci(7))
