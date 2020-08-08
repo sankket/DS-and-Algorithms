4bdb03cfd0c5dadb2c60ab8ae6e6fb9391067545
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-08 15:45:49 +0530

    Update String_Reversal.py

--- Recursion/String_Reversal.py
+++ Recursion/String_Reversal.py
@@ -1,1 +1,17 @@
+string1 = str(input("Enter a string to reverse :"))
 
+
+def reverse_string(string):
+    string1 = ''
+    for i in string:
+        string1 = i + string1
+    return string1
+
+
+print("The original string : ", string1)
+
+print("The reverse of the string: ", reverse_string(string1))
+
+string2 = "my name is godbole"
+
+print(string2[::-1])
