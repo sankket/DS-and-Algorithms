e384385571bd91039082f79cb0b5f4a30aa64595
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-08 15:42:33 +0530

    Code Updated.

--- Recursion/Fibonacci_upto_n.py
+++ Recursion/Fibonacci_upto_n.py
@@ -1,1 +1,29 @@
+# Fibonacci Series - 1,2,3,5,8,13,....
+
+
+def fibonacci():
+    n = int(input("Enter the terms :"))
+    num1 = 0
+    num2 = 1
+    count = 0
+    if n <= 0:
+        print("Enter the valid number")
+    if n == 1:
+        print("Fibonacci sequence upto :")
+        print(num1)
+    else:
+        print("Fibonacci Sequence")
+        while count < n:
+            print(num1)
+            temp = num1 + num2
+            num1 = num2
+            num2 = temp
+            count += 1
+
+
+print(fibonacci())
+
+
+
+
 
