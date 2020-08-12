26bf45aba14909c08855672ce3586cc20bd5ef90
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-12 14:38:25 +0530

    Optimized Method Added.

--- Sorting Algorithms/bubble_sort.py
+++ Sorting Algorithms/bubble_sort.py
@@ -18,3 +18,22 @@ arr = [23,2,43,22,1,56,76]
 print(bubble_sort(arr))
 sorted_arr = [1,2,3,4,5,6]
 print(bubble_sort(sorted_arr))
+
+def bubble_sort_opt(array):
+    count = 0
+    for i in range(len(array)-1):
+        swap = False
+        print(array)
+        for j in range(len(array)-i-1):
+            count += 1
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                swap = True
+            if swap is False:
+                return f'{array} \n Number of comparisons = {count}'
+
+    return f'{array} \n Number of comparisons = {count}'
+
+
+sorted_arr = [1,2,3,4,5,6]
+print(bubble_sort_opt(sorted_arr))
