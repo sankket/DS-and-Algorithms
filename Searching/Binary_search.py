9a077135a575d3ce3bda76a9f182954688099ba1
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-13 15:13:50 +0530

    Code Updated.

--- Searching/Binary_search.py
+++ Searching/Binary_search.py
@@ -1,1 +1,30 @@
+# Binary Search follows divide and Conquer approach.
+
+pos = -1
+
+
+def binary_search(arr, num):
+
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == num:
+            globals()['pos'] = mid
+            return True
+
+        else:
+            if arr[mid] < num:
+                low = mid + 1
+            else:
+                high = mid - 1
+    return False
+
+
+array = [1, 5, 6, 11, 12, 22, 23]
+n = 1
+
+print(binary_search(array, n))
+
 
