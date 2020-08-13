411db9e1865671233330d96f044c23c70f778feb
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-13 15:12:27 +0530

    Code Updated.

--- Searching/Linear_search.py
+++ Searching/Linear_search.py
@@ -1,1 +1,16 @@
+# Linear Search is basically simplest searching algorithm in which
+# The target element will matched with all the element in the list.
+
+
+def linear_search(arr, num):
+    for i in range(len(arr)):
+        if arr[i] == num:
+            return "The Target Found"
+    else:
+        return "Target is not Present"
+
+
+array = [1, 4, 3, 27, 9, 7, 55, 66]
+
+print(linear_search(array, 4))
 
