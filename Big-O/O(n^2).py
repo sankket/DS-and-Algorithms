340a8c58e5a99feed3c713f616fab38583cc625a
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-22 16:55:50 +0530

    Code Updated.

--- Big-O/O(n^2).py
+++ Big-O/O(n^2).py
@@ -2,11 +2,12 @@ import time
 
 array = ['a','b','c','d','e']
 
-def log_all_pairs(array):
+def log_pairs(array):
     for i in range(len(array)): 
         for j in range(len(array)): 
             print(array[i], array[j]) 
 
-log_all_pairs(array)
+log_pairs(array)
 
-# The code contains nested loops hence the complexity is O(n^2)
+# pairs will be printed as the loops will run all over the array.
+# The code contains nested loops hence the complexity is O(n^2).
